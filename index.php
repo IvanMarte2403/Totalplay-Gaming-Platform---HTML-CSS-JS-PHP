58efc2c4342a4608b8d29f9b709d3d9f99e4e589
IvanMarte <ivanbiasfx@gmail.com>
2024-05-04 01:46:43 -0600

    Formulario Change Forms

--- index.php
+++ index.php
@@ -24,7 +24,10 @@
         </div>
 
         <div class="container-register">
-            <div class="container-forms">
+
+            <!-- =======Formulario de Inicio de Sesión======= -->
+
+            <div id="login-form" class="container-forms" >
                 <h2> Iniciar Sesión </h2>
 
                 <form action="login.php" method="post">
@@ -39,14 +42,35 @@
                     
                     <input class="boton-iniciar-sesion" type="submit" value="Iniciar sesión">
                 </form>
-                <p>¿No tienes una cuenta? <a href="registro.php">Regístrate aquí</a>.</p>
+               
             </div>
+
+            <!-- =======Formulario de Registro ======= -->
+            <div id="register-form" class="container-forms" style="display: none;">
+                <h2> Registro </h2>
+
+                <form id="registro" method="post" action="forms.php">
+                    <input type="text" name="nombre_apellidos" placeholder="Nombre y apellidos" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="password" name="contrasena" placeholder="Contraseña" required>
+                    <input type="text" name="genero" placeholder="Genero" required>
+                    <input type="text" name="celular" placeholder="Celular" required>
+                    <input type="date" name="fecha_nacimiento" placeholder="Fecha de Nacimiento" required>
+                    <input type="text" name="numero_cliente_totalplay" placeholder="Número de Cliente Totalplay" required>
+                    <input type="submit" value="Registrarse">
+                </form>
+            </div>
+
+            <!-- ========Enlace de Registro o Inicio de Sesión============ -->
+            <p id="toggle-text">¿No tienes una cuenta? <a id="toggle-link" href="#" onclick="toggleForm()">Regístrate aquí</a>.</p>
+
         </div>
            
         </div>
 
     </div>
 
-  
+    <script src="main/formulario-change-forms.js"></script>
+
 </body>
 </html>
