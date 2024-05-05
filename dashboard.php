f1eb7f012e5b2971359ed777bd26f9636476a4c2
IvanMarte <ivanbiasfx@gmail.com>
2024-05-05 15:40:32 -0600

    Front DashBoard User, Back Redirecciones

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,48 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // Si no ha iniciado sesión, redirigir a la página de inicio (index.php)
+    header('Location: index.php');
+    exit;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+
     <title>TotalPlayGaming</title>
+    
 </head>
 <body>
-    
+    <div class="container">
+        <div class="player-container">
+
+            <div class="info-player">
+                <div class="photo-container">
+                    <img src="img/perfil/temporada1-halo.png" alt="">
+                </div>
+
+                <div id="infoUsuarioContainer" class="info-usuario">
+                    <h2>Ivan </br> Marte</h2>
+                    <p>#123123</p>
+                </div>
+
+            </div>
+
+        </div>
+        
+        <div class="dashboard-container">
+
+        </div>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
+// Iniciar la sesión
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $contrasena = $_POST['contrasena'];
 
-  
     include 'db_conexion.php';
-
 
     $sql = "SELECT * FROM usuarios WHERE email = ? AND contrasena = ?";
     $stmt = $conexion->prepare($sql);
@@ -15,10 +16,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         // Inicio de sesión exitoso
-        echo 'Inicio de sesión Exitoso';
+        $_SESSION['loggedin'] = true;
+
+        // Redirigir al usuario a dashboard.php
+        header('Location: dashboard.php');
+        exit;
     } else {
         // Error de inicio de sesión
-
         echo 'Error de Inicio de Sesion';
     }
 
