cd0a55f3c90d9d7a80ae3da7f2670dfa5017d434
IvanMarte <ivanbiasfx@gmail.com>
2024-05-06 12:59:14 -0600

    Db_conexion CHnage

--- db_conexion.php
+++ db_conexion.php
@@ -1,7 +1,16 @@
-<?php
+<!-- <?php
 $conexion = new mysqli('localhost', 'root', '', 'totalplay-dashboard');
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 ?>
+ -->
+
+<?php
+$conexion = new mysqli('localhost', 'totalplaygamingc_admin', '{!)YY6E8dG?0', 'totalplaygamingc_totalplay-dashboard');
+
+if ($conexion->connect_error) {
+    die("Conexion fallida: " . $conexion->connect_error);
+}
+?> 
