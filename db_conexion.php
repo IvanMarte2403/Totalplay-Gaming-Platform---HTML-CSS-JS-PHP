63baf924ae840dc912da0e8935dd1158d64085a2
IvanMarte <ivanbiasfx@gmail.com>
2024-05-06 13:01:28 -0600

    Change db_conexion

--- db_conexion.php
+++ db_conexion.php
@@ -1,11 +1,4 @@
-<!-- <?php
-$conexion = new mysqli('localhost', 'root', '', 'totalplay-dashboard');
 
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
-}
-?>
- -->
 
 <?php
 $conexion = new mysqli('localhost', 'totalplaygamingc_admin', '{!)YY6E8dG?0', 'totalplaygamingc_totalplay-dashboard');
