66e4233ba0216220d4fe5d08122ffca6c077e248
IvanMarte <ivanbiasfx@gmail.com>
2024-05-06 13:25:36 -0600

    Cpanel Command

--- dashboard.php
+++ dashboard.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/portada-juegos.css">
     <link rel="stylesheet" href="style/dashboard.css">
+    <link rel="stylesheet" href="style/responsive/responsive-dashboard.css">
     <link rel="stylesheet" href="style/casousel.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
