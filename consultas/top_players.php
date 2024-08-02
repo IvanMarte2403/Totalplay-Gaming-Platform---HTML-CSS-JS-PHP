038de01aa5aef3dc43a3f33c6fde8607b742a25e
IvanMarte <ivanbiasfx@gmail.com>
2024-08-01 21:17:25 -0600

    update: weekly objetives

--- consultas/top_players.php
+++ consultas/top_players.php
@@ -2,6 +2,35 @@
 // Incluir el archivo de conexión a la base de datos
 include 'db_conexion.php';
 
+// Obtener el ID del usuario que inició sesión (asumiendo que está almacenado en una variable de sesión)
+$user_id = $_SESSION['id'];
+
+// Obtener el mes y año actual
+$current_month = date('m');
+$current_year = date('Y');
+
+// Inicializar la variable de progreso
+$progreso = 0;
+
+// Consultar y sumar los puntajes de las tablas juego_1, juego_2, juego_4, juego_5
+$tables = ['juego_1', 'juego_2', 'juego_4', 'juego_5'];
+foreach ($tables as $table) {
+    $query = "SELECT SUM(puntaje) as total_puntaje FROM $table WHERE id = ? AND MONTH(fecha_hora) = ? AND YEAR(fecha_hora) = ?";
+    $stmt = $conexion->prepare($query);
+    $stmt->bind_param('iii', $user_id, $current_month, $current_year);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $progreso += $row['total_puntaje'];
+    $stmt->close();
+}
+
+// Definir la meta
+$meta = 100000;
+
+// Calcular el porcentaje de progreso
+$porcentaje = ($progreso / $meta) * 100;
+
 // Consulta para obtener los usuarios con los puntajes más altos
 $query = "SELECT nombre_apellidos, foto_perfil, puntaje_total FROM usuarios ORDER BY puntaje_total DESC LIMIT 3";
 $result = $conexion->query($query);
--- views/dashboard-user.php
+++ views/dashboard-user.php
@@ -1,6 +1,7 @@
 <?php
 include 'consultas/top_players.php';
 
+
 ?>
 
 <div class="dashboard-user-container">
@@ -41,4 +42,30 @@ include 'consultas/top_players.php';
            
         </div>
     </div>
+
+    <div class="dashboard-user-row">
+        <div class="dashboard-objetivo-mensual contenedores-dashboards">
+
+            <h2><i class="fa fa-star"></i> Tu objetivo mensual</h2>
+            <div class="container-numero-proceso">
+                <p> <?php echo number_format($progreso); ?> </p>
+                
+                <p><?php echo number_format($meta); ?></p>
+            </div>
+            
+            <div class="progress-bar-container">
+                
+                <div class="progress-bar" style="width: <?php echo $porcentaje;?>%;"></div>
+               
+
+            </div>
+    </div>
+
+
+        <div class="dashboard-total-puntaje contenedores-dashboards">
+
+        </div>
+    </div>
 </div>
+
+
